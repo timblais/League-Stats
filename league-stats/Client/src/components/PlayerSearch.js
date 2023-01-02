bb1f3f117b2ee4518d4d9f776a630513e958a1b0
Tim Blais <tblais189@gmail.com>
2023-01-01 21:18:54 -0500

    added proxy for backend server calls. added test for player name call to server and console log result

--- league-stats/Client/src/components/PlayerSearch.js
+++ league-stats/Client/src/components/PlayerSearch.js
@@ -3,11 +3,10 @@ import { useState } from 'react';
 
 const PlayerSearch = () => {
     const [playerName, setPlayerName] = useState("");
-    const lolAPIKey = process.env.REACT_APP_LOL_API
 
     const handleSubmit = async (event) => {
         event.preventDefault(); // prevents refresh of the page with submit of the form
-        const response = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${playerName}?${process.env.REACT_APP_LOL_API}`)
+        const response = await fetch(`/api/${playerName}`)
         const json = await response.json()
         console.log(json)
       }
