7bb5d4648616597c3d5052e62a825c326bb3ec96
Tim Blais <tblais189@gmail.com>
2023-01-02 17:55:28 -0500

    continued work on match api requests. Added match lookup and started individual match parsing

--- league-stats/Server/controllers/api.js
+++ league-stats/Server/controllers/api.js
@@ -3,10 +3,30 @@
 module.exports = {
     getPlayer: async (req, res) => {
       try {
-        let response = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.id}?api_key=${process.env.LOL_API_KEY}`)
-        let player = await response.json()
+        // Use incoming player name from client to search for the player and return info including puuid:
+        let getPlayerInfo = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.id}?api_key=${process.env.LOL_API_KEY}`)
+        // returns an object with multiple player properties, including name and puuid
+        let player = await getPlayerInfo.json()
+        let playerName = player.name
+        let puuid = player.puuid
         console.log(player)
-        res.json(player);
+
+        // Use puuid to search for the recent match history of the player
+        let getMatchIdList = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?type=ranked&start=0&count=5&api_key=${process.env.LOL_API_KEY}`)
+        // returns an array of match ids
+        let matches = await getMatchIdList.json()
+        console.log(matches)
+
+        // Create Holding object to house data from matches that we care about
+        let matchesData = {}
+
+        // Loop over the match ids and send api request to get the info for each match. 
+        matches.forEach(async matchId => {
+          let getMatchData = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${process.env.LOL_API_KEY}`)
+          //next step: parse through the match data to pull pertinent info and organize into easy to digest objects for front end to use
+        });
+
+        res.json({fullPlayer: player});
       } catch (err) {
         console.log(err);
       }
